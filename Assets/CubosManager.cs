5eec9ad826bf291e8219ed70aaf05898a7640d56
gonzareyb <br0004@est.ort.edu.ar>
2025-06-17 09:06:38 -0300

    clase 17/6

---
+++ Assets/CubosManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubosManager : MonoBehaviour
+{
+    public CuboScript[] cuboScripts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < cuboScripts.Length; i++)
+        {
+            Debug.Log(cuboScripts[0].esPersistente);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/repasoFor.cs
+++ Assets/repasoFor.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class repasoFor : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Start is callefhgthe first frame update
     void Start()
     {
         
+        for (int i = 3; i < 100; i += 3)
+        {
+            Debug.Log(i);
+            if (cuboScripts[i]).esPersistente != true)
+                    {
+            Destroy(cuboScripts)[i].gameObject);
+        }
+        }
+
     }
 
     // Update is called once per frame
